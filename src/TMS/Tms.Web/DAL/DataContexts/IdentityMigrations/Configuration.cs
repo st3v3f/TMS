513ef426f00a7245865469fea100746b6b1c16fb
Steve Fulton <st3v3f@gmail.com>
2015-07-12 14:48:57 +0100

    Seed an admin user in Identity db.

--- src/TMS/Tms.Web/DAL/DataContexts/IdentityMigrations/Configuration.cs
+++ src/TMS/Tms.Web/DAL/DataContexts/IdentityMigrations/Configuration.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Tms.Web.Models;
+
 namespace Tms.Web.DAL.DataContexts.IdentityMigrations
 {
     using System;
@@ -15,6 +19,15 @@ namespace Tms.Web.DAL.DataContexts.IdentityMigrations
 
         protected override void Seed(Tms.Web.DAL.DataContexts.IdentityDb context)
         {
+
+            if (!(context.Users.Any(u => u.UserName == "admin@tms.com")))
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var userToInsert = new ApplicationUser { UserName = "admin@tms.com", PhoneNumber = "01212342345" };
+                userManager.Create(userToInsert, "Password@456");
+            }
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
